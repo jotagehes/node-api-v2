3eec1607d361e2d765aadfc6a39439c021cb716f
João Gabriel <joaogabriel-12@hotmail.com>
2023-12-04 22:51:10 -0300

    feat: first version

--- atestes/app.js
+++
@@ -1,6 +0,0 @@
-// const lib = require ('./lib.js')
-// console.log (lib.soma (2, 2))
-
-
-const { soma, mult } = require('./lib.js')
-console.log (soma(3, 5))
--- atestes/app2.js
+++
@@ -1,3 +0,0 @@
-import  pi from './lib2.js'
-
-console.log (pi.soma(2, 3))
